d4fdaf1d0b34259d245de7bfc62d37d0e94fbf42
Kem-dot <79644944+Kem-dot@users.noreply.github.com>
2021-03-01 16:54:12 +0600

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ This is my first page
 0010101010101001010101001
 <div class="image-wrap">
 	<img src="pic.png" width="250" height="250" class="image-center" />
+	<a href="https://www.youtube.com/watch?v=n88MReEC27k">click</a>
 </div>
 
 </body>
