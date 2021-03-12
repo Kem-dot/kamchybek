b10fdef05bdc0774b7aa64bf8f72147a7c444c2e
Kem-dot <79644944+Kem-dot@users.noreply.github.com>
2021-03-12 13:20:32 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,58 +1,216 @@
 <!DOCTYPE html>
 <html>
- <head>
-
-<title>tumirlon</title>
- <link rel="stylesheet" type="text/css" href="css/style.css" />
-   <style>
-   .rightpic {
-    float: right; /* Выравнивание по правому краю */
-    margin: 0 0 5px 5px; /* Отступы вокруг фотографии */
-   }
-  </style>
-
- <link rel="stylesheet" type="text/css" href="css/style.css" />
-   <style>
-   .rightpic1 {
-    float: right; /* Выравнивание по правому краю */
-    margin: 0 0 5px 5px; /* Отступы вокруг фотографии */
-   }
-  </style> 
-
- 
-  <body background="https://im0-tub-com.yandex.net/i?id=859329baa1ab234b129edc13ae0a6dc4&n=13" >
-
- </head>
- <p>
-    	<img src="https://yt3.ggpht.com/ytc/AAUvwnhXw7cZDSy_VD577yp5QIJyDJ5gk6RFZr8pfTW_=s900-c-k-c0x00ffffff-no-rj" alt="iaau" width="200"; height="200" style=" vertical-align:right; align-content: right; border-radius: 50%; border="2 class="rightpic">
- 
-
-     </p>
-  	<h1 style="color: #8F9152" align="center" ><i>About Me</i></h1>
-  	<h2 style="color: #8F9152" ><em >NAME:</em> </h2>
-  	<h2 style="color: #B0B7C6" ><em >Kamchybek</em> </h2>
-    <h2 style="color: #8F9152" ><em >SURNAME:</em> </h2>
-    <h2 style="color: #B0B7C6" ><em>SURNAME:Mederbekov</em></h2>
-    <h2 style="color: #8F9152" ><em >SOCIAL STATUS:</em> </h2>
-    <h2 style="color: #B0B7C6" ><em>Student</em></h2>
-    <h2 style="color: #8F9152" ><em >AGE:</em> </h2>
-    <h2 style="color: #B0B7C6" ><em>12*33-378=??years</em></h2>
-    <b>
-   
-
-     <h2 style="color: #8F9152" >Me in social media</h2>
-
-     <a href="https://www.instagram.com/accounts/onetap/?next=%2F">
-     	<img alt="Qries" src="https://im0-tub-com.yandex.net/i?id=46ea1ce0304acedc771520fe6c0af2fc&n=13"
+<head>
+<style>
+body {
+   background-image: url('https://toppng.com/uploads/preview/textured-backgrounds-1920x1080-11553971248tjcgmjojzo.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+}
+</style>
+  <title>KM</title>
+</head>
+<body >
+  <div><img src="http://samp.waptrick.org/samp/w/200x160/38279.jpg" width="150" height="150" style="vertical-align:right; margin-top:0px;  border-radius: 50%; float: right;" border="2";>
+  </div>
+  <div>
+    <h1 style="color: #8F9152" ><em> Kamchybek</em> </h1>
+      <h1 style="color: #8F9152" ><i>Mederbekov</i></h1>
+  </div>
+<div>
+  <h3 style="color:green;">Social Status: <br>Student</br></h3>
+  <h3 style="color:orange;">Age: 18 years</h3>
+  <h4 style="color:pink;">Phone number: +996-507-006-903 </h4>
+       <a href="https://www.instagram.com/accounts/onetap/?next=%2F">
+      <img alt="Qries" src="https://im0-tub-com.yandex.net/i?id=46ea1ce0304acedc771520fe6c0af2fc&n=13"
          width="40" height="40" border="2" >
     
       <a href="https://youtube.com/">
          <img alt="Qries" src="https://www.freeiconspng.com/uploads/youtube-icon-block-png-17.png"
          width="40" height="40" style="color: black" border="2">
   
+ <br>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+/* Extra styles for the cancel button */
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+  position: relative;
+}
+
+img.avatar {
+  width: 40%;
+  border-radius: 50%;
+}
+
+.container {
+  padding: 16px;
+}
+
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  padding-top: 60px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button (x) */
+.close {
+  position: absolute;
+  right: 25px;
+  top: 0;
+  color: #000;
+  font-size: 35px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: red;
+  cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+  -webkit-animation: animatezoom 0.6s;
+  animation: animatezoom 0.6s
+}
+
+@-webkit-keyframes animatezoom {
+  from {-webkit-transform: scale(0)} 
+  to {-webkit-transform: scale(1)}
+}
+  
+@keyframes animatezoom {
+  from {transform: scale(0)} 
+  to {transform: scale(1)}
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+  .cancelbtn {
+    
+     width: 100%;
+  }
+}
+</style>
+</br>
+</head>
+<body>
+
+<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+
+<div id="id01" class="modal">
+  
+  <form class="modal-content animate" action="/action_page.php" method="post">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+        
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>    
+
+
+
+
+
+
+
+
 
 
 
 
 </body>
-</html> 
+</html>
