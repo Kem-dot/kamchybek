cf07ac523ed5953260caaaf74552faa131ffb7cc
Kem-dot <79644944+Kem-dot@users.noreply.github.com>
2021-04-08 19:46:51 +0600

    Update singnup.php

--- singnup.php
+++ singnup.php
@@ -1,1 +1,11 @@
-<br>>
+<?php
+  require "db.php":
+  ?>
+
+  <form action="/singnup" method="POST">
+  	
+  	<p>
+  		<p><strong>Ваш Логин</strong></p>
+  		<input type="text" name="login">
+  	</p>
+  </form>
